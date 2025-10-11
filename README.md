1879f11f1b957e8e9d311129a2c9e81ac6e8d0f5
tristanspear <tristanspear01@gmail.com>
2025-10-10 22:36:00 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# mit-open-courseware-website
+A blog-style website that allows students to easily access MIT's free courses for different STEM subjects.
+
+Check out the site here: https://mit-open-courseware-website.vercel.app
---
+++ index.js
@@ -0,0 +1,88 @@
+import dotenv from "dotenv";
+import express from "express";
+import nodemailer from "nodemailer";
+import path from "path";
+
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+const links = [
+  "https://ocw.mit.edu/search/?d=Mathematics",
+  "https://ocw.mit.edu/search/?d=Physics",
+  "https://ocw.mit.edu/search/?d=Chemistry",
+  "https://ocw.mit.edu/search/?d=Electrical%20Engineering%20and%20Computer%20Science",
+  "https://ocw.mit.edu/search/?d=Engineering%20Systems%20Division&d=Chemical%20Engineering&d=Mechanical%20Engineering&d=Biological%20Engineering&d=Electrical%20Engineering%20and%20Computer%20Science&d=Nuclear%20Science%20and%20Engineering&d=Materials%20Science%20and%20Engineering&d=Civil%20and%20Environmental%20Engineering",
+  "https://ocw.mit.edu/search/?d=Biology",
+];
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("home.ejs"); 
+});
+
+app.get("/game", (req, res) => {
+  res.render("game.ejs");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about.ejs");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
+});
+
+app.post("/frame", (req, res) => {
+  const buttonIdx = req.body.idx;
+  const courseLink = links[buttonIdx];
+  console.log(`POST Request recieved, index : ${buttonIdx}, link: ${courseLink}`);
+
+  res.render("frame.ejs", {
+    mitLink : courseLink,
+  });
+});
+
+app.post("/contact-me", async (req, res) => {
+  const { name, email, message } = req.body;
+
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    await transporter.sendMail({
+      from: email,
+      to: "tspear1704@gmail.com",
+      subject: `MIT Site Contact Form Email from ${name}`,
+      text: message,
+      replyTo: email,
+    });
+
+    // res.status(200).send("Message sent successfully!");
+    res.send(`
+      <script>
+        alert("Message sent successfully!");
+        window.location.href = "/contact";
+      </script>
+    `);
+    
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error sending message.");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
